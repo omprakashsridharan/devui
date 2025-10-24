85780dbc3cddd086581a855597157b1c6aed13b7
Omprakash Sridharan <omprakash.sridharan@sumup.com>
2025-10-24 22:39:08 +0200

    remove postgres.rs

--- src/services/sql/mod.rs
+++ src/services/sql/mod.rs
@@ -1,5 +1,4 @@
 pub mod connection_manager;
-pub mod postgres;
 
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
@@ -47,40 +46,3 @@ impl Config {
         self
     }
 }
-//
-// /// Database introspection trait
-// #[async_trait::async_trait]
-// pub trait DatabaseIntrospector {
-//     /// Get all tables in the database
-//     async fn get_tables(&self) -> Result<Vec<TableInfo>, Box<dyn std::error::Error + Send + Sync>>;
-//
-//     /// Get table schema informationq
-//     async fn get_table_schema(
-//         &self,
-//         table_name: &str,
-//     ) -> Result<TableInfo, Box<dyn std::error::Error + Send + Sync>>;
-//
-//     /// Execute a query and return results
-//     async fn execute_query(
-//         &self,
-//         query: &str,
-//     ) -> Result<QueryResult, Box<dyn std::error::Error + Send + Sync>>;
-// }
-//
-// /// Query execution result
-// #[derive(Debug, Clone, Serialize, Deserialize)]
-// pub struct QueryResult {
-//     pub columns: Vec<String>,
-//     pub rows: Vec<Vec<serde_json::Value>>,
-//     pub row_count: usize,
-// }
-
-// impl QueryResult {
-//     pub fn new() -> Self {
-//         Self {
-//             columns: Vec::new(),
-//             rows: Vec::new(),
-//             row_count: 0,
-//         }
-//     }
-// }
